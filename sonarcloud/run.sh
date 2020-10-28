43726413fad9c9c2d541dd79259cac0412107bc4
ullisesdiaz <72839587+ullisesdiaz@users.noreply.github.com>
2020-10-28 11:24:48 -0600

    Update run.sh

--- sonarcloud/run.sh
+++ sonarcloud/run.sh
@@ -6,7 +6,7 @@ VERSIONANALISIS=$1
 echo "======= Instalando Sonar Scaner Path: $ROOTPATH"
 sudo dotnet tool install --tool-path=$ROOTPATH dotnet-sonarscanner --version 4.10.0
 echo "======= Inicializando análisis Versión: $VERSIONANALISIS"
-sudo dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$VERSIONANALISIS
+sudo dotnet sonarscanner begin /o:ullisesdiaz /k:POC_RD_Repo /d:sonar.host.url=https://sonarcloud.io /version:$VERSIONANALISIS
 echo "======= Compilando código"
 sudo dotnet build src --configuration Release
 echo "======= Finalizando análisis"
